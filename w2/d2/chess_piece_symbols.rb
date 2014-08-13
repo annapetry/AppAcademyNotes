77e2185b455acf6644280753b51def0282e9e768
Anna Petry <hello@annapetry.com>
2014-08-13 08:55:02 -0700

    Add w2 files/gifs. Begin refator of chess files

---
+++ w2/d2/chess_piece_symbols.rb
@@ -0,0 +1,30 @@
+
+
+checkmark = "\u2713"
+
+# white:
+king    = "\u2654"
+queen   = "\u2655"
+rook    = "\u2656"
+bishop  = "\u2657"
+knight  = "\u2658"
+pawn    = "\u2659"
+
+# black
+king    = "\u265A"
+queen   = "\u265B"
+rook    = "\u265C"
+bishop  = "\u265D"
+knight  = "\u265E"
+pawn    = "\u265F"
+
+
+
+
+puts checkmark
+puts king
+puts queen
+puts rook
+puts bishop
+puts knight
+puts pawn
---
+++ w2/d2/sliding_piece.rb
@@ -0,0 +1,28 @@
+require './piece'
+
+
+class SlidingPiece < Piece
+
+  def moves(direction)
+    valid_moves = []
+    direction.each do |dx, dy|
+      counter = 1
+      loop do 
+        next_square = [pos[0] + (dx * counter), pos[1] + (dy * counter)]
+        break if !Board.on_board?(next_square)
+        if @board[next_square].nil?
+          valid_moves << next_square
+          
+        elsif @board[next_square].color == self.color
+          break
+        else
+          valid_moves << @board[next_square]
+          break
+        end
+        counter += 1
+      end
+    end
+    valid_moves
+  end
+end
+

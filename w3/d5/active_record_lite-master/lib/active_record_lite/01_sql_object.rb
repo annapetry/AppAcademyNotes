ecd52de25f74a748264f68e8f95e0cee41ba5846
Anna Petry <hello@annapetry.com>
2014-08-22 16:04:13 -0700

    table_name getter and setters, columns list, variable getter and setters, initialize, all, parse_all, find, insert: all pass specs

--- w3/d5/active_record_lite-master/lib/active_record_lite/01_sql_object.rb
+++ w3/d5/active_record_lite-master/lib/active_record_lite/01_sql_object.rb
@@ -5,42 +5,103 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    cols = DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        "#{table_name}"
+    SQL
+
+    cols[0].map do |var|
+      var.to_sym
+    end
   end
 
   def self.finalize!
+   columns.each do |variable|
+      define_method("#{variable}") do
+        attributes[variable]
+      end
+
+      define_method("#{variable}=") do |value|
+        attributes[variable] = value
+      end
+
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    if self.to_s == "Human"
+      @table_name = "humans"
+    else
+      @table_name = self.to_s.downcase.pluralize
+    end
   end
 
   def self.all
-    # ...
+    result_array = DBConnection.execute(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{table_name}
+    SQL
+
+    self.parse_all(result_array)
   end
 
   def self.parse_all(results)
-    # ...
+    results.map do |result|
+      self.new(result)
+    end
   end
 
   def self.find(id)
-    # ...
+    result_array = DBConnection.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        #{table_name}
+      WHERE
+      #{table_name}.id = ?
+    SQL
+
+    self.parse_all(result_array).first
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def insert
-    # ...
+    col_names_num = self.class.columns.count
+    col_names = self.class.columns.join(",")
+    question_marks = (["?"] * col_names_num).join(",")
+
+    DBConnection.execute(<<-SQL, *attribute_values)
+      INSERT INTO
+        #{self.class.table_name} (#{col_names})
+      VALUES
+      (#{question_marks})
+    SQL
+
+    id = DBConnection.last_insert_row_id
+
+    self.send(:id=, id)
   end
 
-  def initialize
-    # ...
+  def initialize(params)
+    params.each do |attr_name, value|
+      sym = attr_name.to_sym
+      if self.class.columns.include?(sym)
+        self.send("#{sym}=", value)
+      else
+        raise Exception.new("unknown attribute '#{attr_name}'")
+      end
+    end
   end
 
   def save
@@ -52,6 +113,9 @@ class SQLObject
   end
 
   def attribute_values
-    # ...
+    self.class.columns.map do |col_name|
+      self.send(col_name)
+    end
   end
+
 end

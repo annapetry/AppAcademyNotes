7a24e2b502d06cae10b2548894c34d9cbe42e1a6
Anna Petry <hello@annapetry.com>
2014-08-22 16:37:27 -0700

    Complete Phase two.

--- w3/d5/active_record_lite-master/lib/active_record_lite/02_searchable.rb
+++ w3/d5/active_record_lite-master/lib/active_record_lite/02_searchable.rb
@@ -3,10 +3,28 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+
+    where_line = params.keys.map do |key|
+      "#{key} = ?"
+    end.join(" AND ")
+
+    vals = params.map do |key, val|
+      val
+    end
+
+    result = DBConnection.execute(<<-SQL, vals)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        #{where_line}
+    SQL
+
+    self.parse_all(result)
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end

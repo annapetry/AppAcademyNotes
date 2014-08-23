724d93dccd54cefe72782a4443635181da9f9773
Anna Petry <hello@annapetry.com>
2014-08-23 15:33:29 -0700

    Complete first Phase of Blogger Tutorial. Can show, create, edit, and destroy articles. Add flash messages.

--- w3/d6/blogger/app/helpers/articles_helper.rb
+++ w3/d6/blogger/app/helpers/articles_helper.rb
@@ -1,2 +1,6 @@
 module ArticlesHelper
+  # Helper method using 'strong parameters'. 'require' and 'permit' allow us to declare which attributes we'd like to accept
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
